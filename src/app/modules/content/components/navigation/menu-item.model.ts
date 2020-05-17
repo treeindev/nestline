920185c7150e42499ef046b3a99f362133bbee93
treeindev <miquel@treeindev.net>
2020-05-17 17:44:14 +0200

    Add navigation menu model

---
+++ src/app/modules/content/components/navigation/menu-item.model.ts
@@ -0,0 +1,34 @@
+export class MenuItem {
+    /**
+     * A reference to the ionicons icon to use.
+     */
+    icon?: string;
+    /**
+     * The name that will be used on the menu.
+     */
+    name: string;
+    /**
+     * Children items for this item.
+     */
+    children?: Array<MenuItem>;
+    /**
+     * The destination link.
+     */
+    link?: string;
+    /**
+     * Point to an external url (absolute).
+     */
+    url?: string;
+    /**
+     * Target used on the link element.
+     */
+    target?: string;
+    /**
+     * Defines if item is active by default.
+     */
+    active?: boolean;
+    /**
+     * If menu contains children, controls if section is expanded.
+     */
+    expanded?: boolean;
+}
