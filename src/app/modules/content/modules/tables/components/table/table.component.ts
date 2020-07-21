dd6898e7fa2d373aa62a8f4d27e6ad4d336fece7
treeindev <miquel@treeindev.net>
2020-07-21 23:09:48 +0200

    Ensure object independence for tables

--- src/app/modules/content/modules/tables/components/table/table.component.ts
+++ src/app/modules/content/modules/tables/components/table/table.component.ts
@@ -6,8 +6,8 @@ import { TableField } from './table.models';
     templateUrl: './table.component.html'
 })
 export class TableComponent implements OnInit {
-    @Input() header: Array<TableField>;
-    @Input() body: Array<Array<TableField>>;
+    @Input() header: Array<TableField> = [];
+    @Input() body: Array<Array<TableField>> = [];
     @Input() class = '';
     @Input() contentClass = '';
     @Input() defaultText = 'No data available';
@@ -15,7 +15,11 @@ export class TableComponent implements OnInit {
     @Input() allowOrdering = false;
     @Input() editableHeader = 'Edit';
 
+    private ascendingOrder = false;
+
     ngOnInit() {
+        // Create new instances of the received objects to ensure independence.
+        this.header = [...this.header];
         this.body = [...this.body];
     }
 
@@ -23,13 +27,15 @@ export class TableComponent implements OnInit {
         if ( !this.allowOrdering ) { return }
         
         this.body.sort( (a,b) => {
-            const left = a[index].value;
-            const right = b[index].value;
+            const left = this.ascendingOrder ? b[index].value : a[index].value;
+            const right = this.ascendingOrder ? a[index].value : b[index].value;
 
             if ( typeof left === "number" && typeof right === "number" ) { return left - right }
             if ( typeof left === "string" && typeof right === "string" ) { return left.toUpperCase() < right.toUpperCase() ? -1 : 1 }
             
             return 0;
         });
+
+        this.ascendingOrder = !this.ascendingOrder;
     }
 }
